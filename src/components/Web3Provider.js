65f130577cd61f95b97486e6c7c213ac5a16e245
harsh <harsh.chouhan010@gmail.com>
2024-07-26 23:42:20 -0700

    harsh commit

--- src/components/Web3Provider.js
+++ src/components/Web3Provider.js
@@ -1,10 +1,1 @@
-import { useAccount } from "wagmi";
 
-const Web3ModalProvider = async () => {
-
-  const _useAccount = useAccount();
-  const data = await _useAccount.connector?.getProvider();
-
-  return data;
-};
-export default Web3ModalProvider;
